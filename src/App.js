e8a3bf7eac0f85f8ec09bfcc1f5dec7385f3fac4
Roshan mestri <97016680+CodeDrinking@users.noreply.github.com>
2022-12-08 18:36:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Routes , Route  } from "react-router-dom"
+import Todos from './components/Todos';
+import Todo from "./components/Todo"
+import SignUpUser from "./components/SignUpUser"
+import Login from './components/Login';
+import UserProfile from "./components/UserProfile"
+import TodosContextProvider from './contexts/TodosContext'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodosContextProvider>
+      <Router>
+        <Routes>
+          {/* <Route path='/auth' element={<NavBar/>}/> */}
+          <Route path='/' element= {<Todos/>}/>
+          <Route path='/todo/:id' element={<Todo/>}/>
+          <Route path='/register' element={<SignUpUser/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/profile' element={<UserProfile/>}/>
+        </Routes>
+      </Router>
+      </TodosContextProvider>
     </div>
   );
 }
---
+++ src/contexts/TodosContext.jsx
@@ -0,0 +1,31 @@
+import {  useEffect ,useState ,useContext ,createContext} from "react";
+import axios from "axios";
+
+export const todosContext = createContext();
+const TodosContextProvider = ({children}) =>{
+    const [todos, setTodos] = useState([])
+    const[user , SetUser] = useState({})
+    const getTodosData=()=>{
+        axios.get('http://18.179.25.47:8000/api/todos').then((res) => {
+            setTodos(res.data);
+          });  
+    }
+    useEffect(() => {
+        getTodosData();
+      }, []);
+    const value = {
+        todos,
+        getTodosData,
+        user,
+        SetUser
+    }
+   
+      
+    return (
+        <todosContext.Provider value={value}>{children}</todosContext.Provider>
+    )
+};
+export const UseTodo =() => useContext(todosContext);
+export default TodosContextProvider
+
+// "build": "GENERATE_SOURCEMAP=false react-scripts build",
